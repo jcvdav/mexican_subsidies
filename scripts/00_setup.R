472092ae655916cb247943a914242c898a7ac6d9
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-03-10 09:02:53 -0800

    Updated setup script to point to google drive

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -2,7 +2,7 @@
 ## Paths to directories ##
 ##########################
 # Check for OS
-sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/","~/Google Drive File Stream/")
+sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/","/Volumes/GoogleDrive/")
 # Path to our emLab's data folder
 data_path <- paste0(sys_path,"Shared drives/emlab/data")
 # Path to this project's folder
