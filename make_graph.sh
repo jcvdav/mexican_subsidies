d670934106f9c91059b23d339df0c1c896cdf6f0
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-03-10 10:05:07 -0800

    Updated to png

--- make_graph.sh
+++ make_graph.sh
@@ -1,2 +1,2 @@
 #! bin/bash
-LANG=C make -p | python3 make_p_to_json.py | python3 json_to_dot.py | dot -Tpdf >| workflow.pdf
+LANG=C make -p | python3 make_p_to_json.py | python3 json_to_dot.py | dot -Tpng >| workflow.png
