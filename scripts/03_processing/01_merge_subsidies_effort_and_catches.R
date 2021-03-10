9272568c5d13b4aa22efa124feccb1f2b582daa9
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-03-10 09:03:40 -0800

    Updating perging script

--- scripts/03_processing/01_merge_subsidies_effort_and_catches.R
+++ scripts/03_processing/01_merge_subsidies_effort_and_catches.R
@@ -8,11 +8,15 @@ subsidies <-
 
 effort <- readRDS(file.path(project_path, "processed_data", "effort.rds"))
 
-landings <- readRDS(file.path(.....)) 
+landings <- readRDS(file.path(project_path, "processed_data", "")) 
 
-a <- subsidies %>% 
-  left_join(landings, by = c("economic_unit", "year")) %>% 
-  filter(!diesel_liters<100)
+a <- landings %>% 
+  filter(year > 2010) %>% 
+  left_join(subsidies, by = c("economic_unit", "year")) %>% 
+  mutate(year = as.factor(year)) %>% 
+  group_by(year, economic_unit, landed_weight) %>% 
+  summarize(subsidy_amount = sum(subsidy_amount, na.rm = T)) %>% 
+  replace_na(replace = list(subsidy_amount = 0))
 
 ggplot(a, aes(x = diesel_liters, y = landed_weight, color = year)) +
   geom_point(alpha = 0.5) +
@@ -24,5 +28,16 @@ ggplot(a, aes(x = diesel_liters, y = landed_weight, color = year)) +
   facet_wrap(~main_group)
 
 
-feols(log(landed_weight) ~ log(diesel_liters) | economic_unit + year + main_group, data = a) %>% 
+plm(formula = landed_weight ~ subsidy_amount,
+    index = c("economic_unit", "year"), 
+    model = "random",
+    effect = "individual",
+    data = a) %>% 
+  modelsummary::modelsummary(gof_omit = "Pseudo|Adj|With|IC|Log", stars = TRUE, statistic_vertical = FALSE)
+
+
+feols(landed_weight ~ subsidy_amount | economic_unit + year, data = a) %>% 
+  modelsummary::modelsummary(gof_omit = "Pseudo|Adj|With|IC|Log", stars = TRUE, statistic_vertical = FALSE)
+
+lm(landed_weight ~ subsidy_amount + economic_unit + year, data = a) %>% 
   modelsummary::modelsummary(gof_omit = "Pseudo|Adj|With|IC|Log", stars = TRUE, statistic_vertical = FALSE)
