d8e840a7cec1270d01c49c0e98614c31f257e00b
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-04-20 16:41:56 -0700

    Updated economic unit subsidy panel code and data

--- scripts/03_processing/01_create_economic_unit_subsidy_panel.R
+++ scripts/03_processing/01_create_economic_unit_subsidy_panel.R
@@ -32,7 +32,8 @@ economic_unit_panel <- all_fuel_clean %>%
            fill = list(subsidy_pesos = 0)) %>%                          #      filling it in with zeroes
   mutate(subsidy_pesos_pv = subsidy_pesos / n_large_scale_vessels,      # Calculate the per-vessel subsidy amount
          subsidy_liters = subsidy_pesos / 2,                            # Convert pesos to liters
-         subsidy_liters_pv = subsidy_pesos_pv / 2)                      # Conver per vessel pesos to liters
+         subsidy_liters_pv = subsidy_pesos_pv / 2) %>%                  # Convert per vessel pesos to per vessel liters
+  rename(eu_rnpa = rnpa)
 
 # Export the data ##############################################################################################################
 saveRDS(economic_unit_panel,
