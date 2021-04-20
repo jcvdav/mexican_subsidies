5fe9f49b97d47f58d5961f5379f7496bcc2a973a
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-04-20 09:54:47 -0700

    Added documentation to subsidy panel and added unit conversions

--- scripts/03_processing/01_create_economic_unit_subsidy_panel.R
+++ scripts/03_processing/01_create_economic_unit_subsidy_panel.R
@@ -1,13 +1,41 @@
+######################################################
+#         create economic unit subsidy panel         #
+######################################################
 
+# Set up
+## Load packages
 library(here)
 library(tidyverse)
 
+## Load data
 all_fuel_clean <- readRDS(here("data", "all_fuel_clean.rds"))
 
-economic_unit_panel <- all_fuel_clean %>% 
-  group_by(year, rnpa, fishing_type, fuel_type, target, n_large_scale_vessels, n_small_scale_vessels, zone, state, municipality, location) %>% 
-  summarize(subsidy_amount = sum(subsidy_amount, na.rm = T)) %>% 
-  ungroup() %>% 
-  complete(year, nesting(rnpa, fishing_type), fill = list(subsidy_amount = 0))
 
-saveRDS(economic_unit_panel, here("data", "economic_unit_subsidy_panel.rds"))
+economic_unit_panel <- all_fuel_clean %>%
+  filter(fuel_type == "Diesel") %>%                                     # Keep diesel only        
+  group_by(                                                             # Define groups
+    year,
+    rnpa,
+    fishing_type,
+    fuel_type,
+    target,
+    n_large_scale_vessels,
+    n_small_scale_vessels,
+    zone,
+    state,
+    municipality,
+    location
+  ) %>%
+  summarize(subsidy_pesos = sum(subsidy_amount, na.rm = T)) %>%         # Calculate total subsidy amount for each economic unit
+  ungroup() %>%                                                         # Undo groups
+  complete(year, nesting(rnpa, fishing_type),                           # Balance the panel
+           fill = list(subsidy_pesos = 0)) %>%                          #      filling it in with zeroes
+  mutate(subsidy_pesos_pv = subsidy_pesos / n_large_scale_vessels,      # Calculate the per-vessel subsidy amount
+         subsidy_liters = subsidy_pesos / 2,                            # Convert pesos to liters
+         subsidy_liters_pv = subsidy_pesos_pv / 2)                      # Conver per vessel pesos to liters
+
+# Export the data
+saveRDS(economic_unit_panel,
+        here("data", "economic_unit_subsidy_panel.rds"))
+
+# END OF SCRIPT
