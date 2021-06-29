b13e53d5198960d1439ee8a55bf7ba5805df14a2
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-06-29 12:26:54 -0700

    Adding mex fisheries path, and ggplot geom defaults to setup script

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -7,6 +7,8 @@ sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/","/Volumes/GoogleDrive
 data_path <- paste0(sys_path,"Shared drives/emlab/data")
 # Path to this project's folder
 project_path <- paste0(sys_path,"Shared drives/emlab/projects/current-projects/mexican-subsidies")
+# Path to all Mexican data
+mex_path <- paste0(sys_path,"Shared drives/emlab/projects/current-projects/mex-fisheries")
 
 # Reset theme
 ggplot2::theme_set(startR::ggtheme_plot())
@@ -17,6 +19,12 @@ ggplot2::theme_update(
   axis.text.y = ggplot2::element_text(size = 12),
   axis.text.x = ggplot2::element_text(size = 12)
 )
+
+ggplot2::update_geom_defaults(geom = "point",
+                              new = list(color = "black",
+                                         fill = "steelblue",
+                                         shape = 21,
+                                         size = 2))
 
 
 # Turn off dplyr's anoying messages
