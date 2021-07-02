711e7fbb2217d5c4d85dc8d3ce39d593065982c4
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-02 11:18:34 -0700

    Update panel creation, with fuel-specific prices and documentation

--- scripts/03_processing/03_create_regression_panel.R
+++ scripts/03_processing/03_create_regression_panel.R
@@ -17,19 +17,26 @@ library(tidyverse)
 ## Read data
 # Fuel consumptiom
 fuel_consumption_raw <-
-  readRDS(here("data", "vms_monthly_fuel_consumption.rds"))
+  read.csv(file.path(project_path, "data", "processed_data", "vms_annual_fuel_consumption.csv"),
+           stringsAsFactors = F)
 
 # Subsidy panel
 subsidy_panel_raw <-
-  tibble(readRDS(here("data", "economic_unit_subsidy_panel.rds")))
+  read.csv(file.path(project_path, "data", "processed_data", "economic_unit_subsidy_panel.csv"),
+           stringsAsFactors = F)
 
-diesel_prices <- read.csv(file.path(project_path, "raw_data", "monthly_diesel_prices.csv")) %>% 
+fuel_prices <- read.csv(file.path(project_path, "data", "raw_data", "monthly_diesel_prices.csv")) %>% 
   select(month = Month, price = Price) %>% 
   mutate(date = lubridate::mdy(month),
          month = lubridate::month(date),
-         year = lubridate::year(date)) %>% 
+         year = lubridate::year(date),
+         fuel_type = "Diesel") %>% 
+  group_by(year) %>% 
+  mutate(mean_price = mean(price, na.rm = T)) %>% 
+  ungroup() %>% 
+  rename(july_price = price) %>% 
   filter(month == 06) %>% 
-  select(year, ph = price)
+  select(-c(month, date))
 
 ## PROCESSING ######################################################################################################################################
 subsidy_panel <- subsidy_panel_raw %>% 
@@ -37,57 +44,34 @@ subsidy_panel <- subsidy_panel_raw %>%
   select(year,
          eu_rnpa,
          contains("subsidy"),
+         fuel_type,
          treated,
          applied,
          zone)
 
 fuel_consumption <- fuel_consumption_raw %>%
-  group_by(eu_rnpa, year) %>% 
+  group_by(year, eu_rnpa) %>% 
   summarize(fuel_consumption_l = sum(fuel_consumption_l, na.rm = T),
             fuel_consumption_max_l = sum(fuel_consumption_max_l, na.rm = T),
-            h = sum(h, na.rm = T)) %>% 
+            hours = sum(h, na.rm = T)) %>% 
   ungroup()
 
 
 panel <- fuel_consumption %>% 
   left_join(subsidy_panel, by = c("eu_rnpa", "year")) %>% 
-  left_join(diesel_prices, by = "year") %>% 
-  # replace_na(replace = list(subsidy_liters = 0, subsidy_liters_pv = 0, treated = F, applied = F)) %>%
-  mutate(pl = ph - 2,
+  left_join(fuel_prices, by = c("year", "fuel_type")) %>% 
+  replace_na(replace = list(subsidy_liters = 0, subsidy_liters_pv = 0, treated = F, applied = F)) %>%
+  mutate(ph = mean_price,
+         pl = ph - 2,
          phi = subsidy_cap_l / fuel_consumption_l,
          D = phi <= 1L,
          R = ph - pl,
-         term1 = pl + (D * R),
-         term2 = (phi * R  * D),
+         c_term1 = pl + (D * R),
+         c_term2 = (phi * R  * D),
          o_term1 = (pl * (1 - D)) + (ph * D),
-         o_term2 = D * phi * (pl - ph)) %>% 
-  add_count(eu_rnpa) %>% 
-  filter(n > 2,
-         treated,
-         !eu_rnpa == "2508008238")
+         o_term2 = D * phi * (pl - ph))
 
 
-model1 <- feols(fuel_consumption_l ~ term1 + term2 | eu_rnpa, data = panel)
-model2 <- feols(fuel_consumption_l ~ term1 + term2 | eu_rnpa + zone, data = panel)
-model3 <- feols(fuel_consumption_l ~ term1 + term2 | eu_rnpa + zone + year, data = panel)
-
-rows <- tibble(a = "alpha", b = get_alpha(model1), c = get_alpha(model2),  d = get_alpha(model3))
-
-models <- list(model1, model2, model3)
-
-modelsummary(models, stars = T,
-             statistic_vertical = F,
-             add_rows = rows,
-             gof_omit = "Adj|With|IC|Log")
-
-ggplot(panel, aes(x = term1, y = fuel_consumption_l)) + 
-  geom_path(aes(group = eu_rnpa))
-
-ggplot(panel, aes(x = term2)) + 
-  geom_histogram()
-
-
-###
-fuel_consumption %>% 
-  left_join(subsidy_panel, by = c("eu_rnpa", "year")) %>% 
-  left_join(diesel_prices, by = "year")
+write.csv(x = panel,
+          file = file.path(project_path, "data", "processed_data", "estimation_panel.csv"),
+          row.names = F)
