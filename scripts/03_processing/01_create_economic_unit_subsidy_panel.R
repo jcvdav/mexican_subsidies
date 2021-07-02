a91304b55199f619d0b10e8acbea9dbddc7ae23d
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-02 10:22:54 -0700

    Renaming variables

--- scripts/03_processing/01_create_economic_unit_subsidy_panel.R
+++ scripts/03_processing/01_create_economic_unit_subsidy_panel.R
@@ -8,11 +8,10 @@ library(here)
 library(tidyverse)
 
 ## Load data #################################################################################################################
-all_fuel_clean <- readRDS(here("data", "all_fuel_clean.rds"))
+fuel_caps <- readRDS(here("data", "economic_unit_subsidy_caps.rds"))
 
 ## Creat the panel ###########################################################################################################
-economic_unit_panel <- all_fuel_clean %>%
-  filter(fuel_type == "Diesel") %>%                                     # Keep diesel only
+economic_unit_panel <- fuel_caps %>%
   group_by(                                                             # Define groups
     year,
     rnpa,
@@ -26,17 +25,17 @@ economic_unit_panel <- all_fuel_clean %>%
     municipality,
     location
   ) %>%
-  summarize(subsidy_pesos = sum(subsidy_amount, na.rm = T)) %>%         # Calculate total subsidy amount for each economic unit
+  summarize(subsidy_cap_pesos = sum(subsidy_amount, na.rm = T)) %>%         # Calculate total subsidy amount for each economic unit
   ungroup() %>%                                                         # Undo groups
   mutate(applied = TRUE,
-         treated = subsidy_pesos > 0) %>% 
-  complete(year, nesting(rnpa, fishing_type),                           # Balance the panel
-           fill = list(subsidy_pesos = 0,                               # filling it in with zeroes
-                       treated = FALSE,                                 # filling it in with FALSE
-                       applied = FALSE)) %>%                            # filling it in with FALSE
-  mutate(subsidy_pesos_pv = subsidy_pesos / n_large_scale_vessels,      # Calculate the per-vessel subsidy amount
-         subsidy_liters = subsidy_pesos / 2,                            # Convert pesos to liters
-         subsidy_liters_pv = subsidy_pesos_pv / 2) %>%                  # Convert per vessel pesos to per vessel liters
+         treated = subsidy_cap_pesos > 0) %>% 
+  complete(year, nesting(rnpa, fishing_type),                                   # Balance the panel
+           fill = list(subsidy_pesos = 0,                                       # filling it in with zeroes
+                       treated = FALSE,                                         # filling it in with FALSE
+                       applied = FALSE)) %>%                                    # filling it in with FALSE
+  mutate(subsidy_cap_l = subsidy_cap_pesos / 2,                                 # Convert pesos to liters
+         subsidy_cap_pv_pesos = subsidy_cap_pesos / n_large_scale_vessels,      # Calculate the per-vessel subsidy amount
+         subsidy_cap_pv_l = subsidy_cap_pv_pesos / 2) %>%                           # Convert per vessel pesos to per vessel liters
   rename(eu_rnpa = rnpa)
 
 # Export the data ##############################################################################################################
