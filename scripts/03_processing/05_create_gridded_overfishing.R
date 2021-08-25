6131b0db929c14a48df2dddd2f5817de612a310c
Juan Carlos Villaseñor-Derbez <juancarlos@uccsb.edu>
2021-08-25 09:14:07 -0700

    Adding script to create gridded overfishing

--- scripts/03_processing/05_create_gridded_overfishing.R
+++ scripts/03_processing/05_create_gridded_overfishing.R
@@ -25,8 +25,8 @@ mex_seas <- st_read(file.path(data_path, "world-seas-v3", "World_Seas_IHO_v3"), 
   st_intersection(mex_eez)
 
 effort_counterfactual <- left_join(effort, overfishing, by = c("year", "eu_rnpa")) %>% 
-  drop_na(alpha) %>% 
-  replace_na(replace = list(pc = 0)) %>%
+  # drop_na(alpha) %>%
+  replace_na(replace = list(pc = 0, alpha = 1)) %>%
   filter(lat_bin_center < 90, lon_bin_center > -360) %>% 
   mutate(additional_fuel_consumption_l = fuel_consumption_l * pct) %>% 
   group_by(year, alpha, lat_bin_center, lon_bin_center) %>% 
@@ -57,7 +57,26 @@ ggplot(effort_counterfactual) +
                                title.position = "top"))  +
   labs(x = "", y = "")
 
-
+effort_counterfactual %>% 
+  filter(pct > 0.5 * max(pct, na.rm = T)) %>% 
+  ggplot() +
+  geom_sf(data = america, fill = "transparent") +
+  geom_sf(data = coast, color = "black") +
+  geom_tile(aes(x = lon_bin_center, y = lat_bin_center, fill = pct)) +
+  geom_sf(data = mex_seas, fill = "transparent", color = "black") +
+  scale_fill_viridis_c(trans = "log10") +
+  facet_grid(~response) +
+  lims(x = c(-150, -70), y = c(-20, 40)) +
+  theme_void(base_size = 14) +
+  theme(legend.position = "bottom") +
+  theme(strip.text.x=element_text(margin=margin(b = 1))) +
+  guides(fill = guide_colorbar(title = "% Effort caused by subsidy\n(log-10 scale)",
+                               direction = "horizontal",
+                               frame.colour = "black",
+                               ticks.colour = "black",
+                               barwidth = 10,
+                               title.position = "top"))  +
+  labs(x = "", y = "")
 
 
 
