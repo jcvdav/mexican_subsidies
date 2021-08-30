60ea4fc9ceedd581eac8ca036746f50fc76b6314
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-08-30 10:19:09 -0700

    Updating fishery-level stats

--- scripts/report/04_fishery_level_stats.R
+++ scripts/report/04_fishery_level_stats.R
@@ -3,38 +3,28 @@ overfishing <- readRDS(file.path(project_path, "data", "output_data", "simulated
   select(year, species, alpha, eu_rnpa, fuel_consumption_l, q_counter, pct, overfishing) %>% 
   mutate(response = ifelse(alpha == 1, "Marginal", "Average"))
 
-overfishing %>% 
-  mutate(species = case_when(species == "shrimp plus" ~ "shrimp",
-                             species == "any" ~ "finfish",
-                             T ~ species)) %>% 
-  group_by(year, species, alpha, response) %>% 
-  summarize(fuel_consumption_l = sum(fuel_consumption_l),
-            q_counter = sum(q_counter),
-            overfishing = sum(overfishing)) %>% 
-  mutate(pct = overfishing / q_counter) %>% 
-  ggplot(aes(x = year, y = pct, linetype = response)) +
-  geom_line() +
-  facet_wrap(~species, scales = "free_y") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(x = "Year", y = "% Additional fuel consumption") +
-  guides(linetype = guide_legend("Response")) +
-  theme(legend.justification = c(1, 1),
-        legend.position = c(1, 1))
 
-overfishing %>% 
+fisheries_plot <- overfishing %>% 
   mutate(species = case_when(species == "shrimp plus" ~ "shrimp",
-                             species == "any" ~ "finfish",
-                             T ~ species)) %>% 
+                             species == "any" ~ "Others",
+                             T ~ species),
+         species = str_to_sentence(species)) %>% 
   group_by(year, species, alpha, response) %>% 
   summarize(fuel_consumption_l = sum(fuel_consumption_l),
             q_counter = sum(q_counter),
             overfishing = sum(overfishing)) %>% 
   mutate(pct = overfishing / q_counter) %>% 
-  ggplot(aes(x = year, y = overfishing / 1e6, linetype = response)) +
+  ggplot(aes(x = year, y = overfishing / 1e3, linetype = response)) +
   geom_line() +
+  geom_point(aes(size = pct), fill = "black") +
   facet_wrap(~species, scales = "free_y") +
-  scale_y_continuous() +
-  labs(x = "Year", y = "Additional fuel consumption\n(Million liters)") +
-  guides(linetype = guide_legend("Response")) +
-  theme(legend.justification = c(1, 1),
-        legend.position = c(1, 1))
+  scale_x_continuous(breaks = seq(2011, 2019, by = 2), labels = seq(2011, 2019, by = 2)) +
+  scale_size_continuous(labels = scales::percent) +
+  labs(x = "Year", y = "Additional fuel consumption\n(Thousand liters)") +
+  guides(linetype = guide_legend("Response"),
+         size = guide_legend("% Change")) 
+
+ggsave(fisheries_plot,
+       filename = file.path(project_path, "results", "figures", "fisheries_subsidy_effect_timeseries.png"),
+       width = 9,
+       height = 5)
