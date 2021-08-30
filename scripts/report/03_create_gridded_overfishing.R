d093a7c0b2ded8125acf7787306edc08e501574a
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-08-30 10:18:42 -0700

    Updating with new figure scripts

--- scripts/report/03_create_gridded_overfishing.R
+++ scripts/report/03_create_gridded_overfishing.R
@@ -15,18 +15,26 @@ overfishing <- readRDS(file.path(project_path, "data", "output_data", "simulated
 america <- rnaturalearth::ne_countries(continent = c("North America", "South America"), returnclass = "sf") %>% 
   st_crop(xmin = -150, ymin = -25, xmax = -70, ymax = 40)
 
-coast <- rnaturalearth::ne_countries(country = "Mexico", returnclass = "sf")
+coast <- rnaturalearth::ne_countries(country = "Mexico", returnclass = "sf", scale = "large") %>% 
+  st_make_valid()
 
 mex_eez <- st_read(file.path(data_path, "marine-regions-eez-v11/World_EEZ_v11_20191118_gpkg/eez_v11.gpkg")) %>% 
-  filter(ISO_TER1 == "MEX") %>% 
-  ms_simplify()
+  filter(ISO_TER1 == "MEX")
 
-mex_seas <- st_read(file.path(data_path, "world-seas-v3", "World_Seas_IHO_v3"), "World_Seas_IHO_v3") %>% 
+seas <- st_read(file.path(data_path, "world-seas-v3", "World_Seas_IHO_v3"), "World_Seas_IHO_v3") %>% 
   filter(NAME %in% c("Gulf of California", "Gulf of Mexico", "North Pacific Ocean", "Caribbean Sea")) %>% 
-  ms_simplify() %>% 
+  st_make_valid()
+
+mex_seas <- seas %>% 
   st_intersection(mex_eez)
 
+mex_seas_viz <- ms_simplify(mex_seas)
+
+high_seas <- seas %>% 
+  st_difference(mex_eez)
+
 effort_counterfactual <- left_join(effort, overfishing, by = c("year", "eu_rnpa")) %>% 
+  filter(year == 2019) %>% 
   replace_na(replace = list(pc = 0, alpha = 1)) %>%
   filter(lat_bin_center < 90, lon_bin_center > -360) %>% 
   mutate(additional_fuel_consumption_l = fuel_consumption_l * pct) %>% 
@@ -39,38 +47,36 @@ effort_counterfactual <- left_join(effort, overfishing, by = c("year", "eu_rnpa"
 
 
 map <- ggplot(effort_counterfactual) +
-  geom_sf(data = america, fill = "transparent") +
+  geom_sf(data = america, fill = "transparent", size = 0.1) +
   geom_tile(aes(x = lon_bin_center, y = lat_bin_center, fill = pct)) +
-  geom_sf(data = mex_seas, fill = "transparent", color = "black") +
+  geom_sf(data = mex_seas_viz, fill = "transparent", color = "black") +
   scale_fill_viridis_c(trans = "log10") +
   facet_grid(~response) +
   theme_void(base_size = 14) +
   scale_x_continuous(expand = c(0, 0), limits = c(-150, -70)) +
   scale_y_continuous(expand = c(0, 0), limits = c(-20, 40)) +
-  theme(#legend.position = "bottom",
+  theme(legend.position = "top",
         strip.text.x=element_text(margin=margin(b = 2)),
         strip.background = element_blank(),
         panel.spacing = unit(10, "pt")) +
   guides(fill = guide_colorbar(title = "% Effort caused by\nsubsidy (log-10 scale)",
-                               # direction = "horizontal",
                                frame.colour = "black",
-                               ticks.colour = "black"#,
-                               # barwidth = 10,
-                               # title.position = "top"
+                               ticks.colour = "black",
+                               barwidth = 10
                                ))  +
   labs(x = "", y = "")
 
 # Zonal stats
 
-a <- effort_counterfactual %>% 
+effort_rasters <- effort_counterfactual %>% 
   select(x = lon_bin_center, y = lat_bin_center, base_fuel_consumption_l = fuel_consumption_l, additional_fuel_consumption_l, response) %>% 
   pivot_wider(names_from = response, values_from = c(base_fuel_consumption_l, additional_fuel_consumption_l), values_fill = 0) %>% 
   rasterFromXYZ(res = 0.1)
 
 seas_zonal_stats <- mex_seas %>% 
   select(name = NAME) %>% 
-  cbind(raster::extract(a, ., fun = "sum", data.frame = T, na.rm = T)) %>% 
-  st_drop_geometry() %>% 
+  cbind(raster::extract(effort_rasters, ., fun = "sum", data.frame = T, na.rm = T)) %>% 
+  st_drop_geometry() %>%
   pivot_longer(cols = contains("fuel"),
                names_to = "layer",
                values_to = "fuel") %>% 
@@ -79,28 +85,22 @@ seas_zonal_stats <- mex_seas %>%
   pivot_wider(names_from = consumption, values_from = fuel) %>% 
   mutate(pct = additional / base)
 
-rel_bar <- ggplot(seas_zonal_stats, aes(x = name, y = pct, fill = response)) +
+abs_bar <- ggplot(seas_zonal_stats, aes(x = name, y = additional / 1e6, fill = response, alpha = pct)) +
   geom_col(position = "dodge") +
   scale_fill_brewer(palette = "Set1") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(x = "Sea", y = "% Additional fuel consumption") +
-  guides(fill = guide_legend("Response")) +
-  theme(legend.position = "None") +
-  coord_flip()
-
-abs_bar <- ggplot(seas_zonal_stats, aes(x = name, y = additional / 1e6, fill = response)) +
-  geom_col(position = "dodge") +
-  scale_fill_brewer(palette = "Set1") +
+  scale_alpha_continuous(labels = scales::percent) +
   scale_y_continuous() +
-  labs(x = "Sea", y = "Additional fuel consumption (million L)") +
-  guides(fill = guide_legend("Response")) +
-  theme(legend.justification = c(1, 0),
-        legend.position = c(1, 0)) +
+  labs(x = "", y = "Additional fuel consumption (million L)") +
+  guides(fill = guide_legend("Response"),
+         alpha = guide_legend("% Change")) +
   coord_flip()
 
+spatial <- plot_grid(map, abs_bar, ncol = 1, rel_heights = c(2, 1))
 
-bars <- plot_grid(rel_bar, abs_bar, ncol = 2)
-plot_grid(map, bars, ncol = 1)
+ggsave(spatial,
+       filename = file.path(project_path, "results", "figures", "spatial_subsidy_footprint.png"),
+       width = 8,
+       height = 6)
 
 
 # Species-level
