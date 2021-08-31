48cf3c1238a671014f99189488c20bbea85cc227
Juan Carlos Villaseñor-Derbez <juancarlos@uccsb.edu>
2021-08-31 16:24:45 -0700

    Updating program redesign

--- scripts/report/06_program_redesign.R
+++ scripts/report/06_program_redesign.R
@@ -3,66 +3,103 @@ library(tidyverse)
 
 source(here::here("scripts", "00_setup.R"))
 
-panel <- read_csv(file.path(project_path, "data", "processed_data", "estimation_panel.csv"), show_col_types = F) %>% 
-  filter(year == 2019) %>% 
-  filter(phi < 10) %>%
-  select(eu_rnpa, species, total_hp, fuel_consumption_l, treated, subsidy_cap_l, ph)
-
 model <- readRDS(file.path(project_path, "data", "output_data", "model.rds"))
 
 response <- model$coefficients
 
-pcts <- seq(0, 2, by = 0.25)
+panel <- read_csv(file.path(project_path, "data", "processed_data", "estimation_panel.csv"), show_col_types = F) %>% 
+  filter(year == 2019) %>% 
+  filter(phi < 10) %>%
+  mutate(eu_rnpa = factor(eu_rnpa)) %>% 
+  # filter(eu_rnpa %in% c("203000922", "203004411")) %>%
+  select(eu_rnpa, species, total_hp, fuel_consumption_l, treated, subsidy_cap_l, ph, pl, phi, D)
+
+pcts <- seq(0, 5, by = 0.25)
 
 sim_panel <- panel %>% 
-  expand_grid(alpha = c(0, 1),
+  expand_grid(alpha = c(1),
               pct_p = pcts,
               pct_q = pcts) %>%
-  mutate(resp = ifelse(alpha == 1, "Marginal", "Average"),
-         pl = ph - (pct_p * 2),
+  mutate(resp = ifelse(alpha == 1, "Marginal", "Average")) %>% 
+  mutate(price_subsidy = -2 * pct_p,
          subsidy_cap_l = pct_q * subsidy_cap_l,
-         phi = subsidy_cap_l / fuel_consumption_l,
-         D = phi <= 1L,
-         R = ph - pl,
-         act_price = (fuel_consumption_l<=subsidy_cap_l)*(treated)*pl + 
-           (fuel_consumption_l<=subsidy_cap_l)*(!treated)*ph +
-           (fuel_consumption_l>subsidy_cap_l)*ph,
-         eu_rnpa = factor(eu_rnpa),
+         p_redesign = ph + price_subsidy,
          pp = ph - ((1 - alpha) * (ph - pl) * phi),
          p = case_when(!treated ~ ph,
                        treated & !D ~ pl,
                        treated & D ~ pp),
          R = ph - p,
-         q_counter = pmax((fuel_consumption_l + (treated * response * 2)), 0),
-         q_redesign = pmax((fuel_consumption_l + (treated * response * R)), 0),
-         q_additonal = q_redesign - q_counter,
+         q_counter = pmax((fuel_consumption_l + (treated * response * R)), 0),
+         q_redesign = pmax((q_counter + (treated * response * price_subsidy)), 0),
+         q_redesign = (ifelse(q_redesign > subsidy_cap_l, q_counter, q_redesign)), 
+         q_additional = q_redesign - q_counter,
          subsidized_fuel_l = pmin(q_counter, subsidy_cap_l),
-         govt_outlay = subsidized_fuel_l * R) 
+         govt_outlay = -price_subsidy * subsidized_fuel_l)
+
+
+# subsidy_cap_l = pct_q * subsidy_cap_l,
+# price_subsidy = -pct_p * 2,
+#          pl = ph - price_subsidy,
+#          q_redesign = pmax((q_counter + (treated * response * price_subsidy)), 0),
+#          phi = subsidy_cap_l / q_counter,
+#          D = phi <= 1L,
+#          pp = ph - ((1 - alpha) * (ph - pl) * phi),
+#          p = case_when(!treated ~ ph,
+#                        treated & !D ~ pl,
+#                        treated & D ~ pp),
+#          R = ph - p,
+#          # q_redesign = pmax((q_counter + (treated * response * R)), 0),
+#          q_additonal = q_redesign - q_counter,
+#          subsidized_fuel_l = pmin(q_counter, subsidy_cap_l),
+#          govt_outlay = subsidized_fuel_l * R) 
+
+
+
+
+# ###
+# 
+# sim_panel <- panel %>% 
+#   expand_grid(alpha = c(0, 1)) %>% 
+#   mutate(pp = ph - ((1 - alpha) * (ph - pl) * phi),
+#          p = case_when(!treated ~ ph,
+#                        treated & !D ~ pl,
+#                        treated & D ~ pp),
+#          R = ph - p,
+#          q_counter = pmax((fuel_consumption_l + (treated * response * R)), 0),
+#          overfishing = fuel_consumption_l - q_counter,
+#          pct = overfishing / fuel_consumption_l) %>% 
+#   filter(fuel_consumption_l > 0) %>% 
+#   select(year, eu_rnpa, species, fuel_consumption_l, hours, subsidy_cap_l, alpha, act_price, p, pp, q_counter, overfishing, pct)
+# 
+# 
+# ###
 
 summarized_simulations <- sim_panel %>% 
   group_by(resp, pct_p, pct_q) %>% 
   summarize(fuel_consumption_l = sum(fuel_consumption_l),
             q_counter = sum(q_counter),
             q_redesign = sum(q_redesign),
-            q_additonal = sum(q_additonal),
+            q_additional = sum(q_additional),
             subsidized_fuel_l = sum(subsidized_fuel_l),
             govt_outlay = sum(govt_outlay)) %>% 
-  mutate(pct_additional = q_additonal / q_counter)
+  mutate(pct_additional = q_additional / q_counter)
 
 
-ggplot(summarized_simulations, aes(x = pct_q, y = pct_p, size = pct_additional, fill = q_additonal / 1e6, z = govt_outlay)) +
+ggplot(summarized_simulations, aes(x = pct_q, y = pct_p, fill = pct_additional, size = govt_outlay / 1e6, z = govt_outlay)) +
   geom_hline(yintercept = 1, linetype = "dashed") +
   geom_vline(xintercept = 1, linetype = "dashed") +
   geom_contour(show.legend = F, color = "black") +
   # geom_text_contour() +
   geom_point(alpha = 0.9) +
-  scale_fill_viridis_c() +
-  scale_size_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_viridis_c(labels = scales::percent) +
+  # scale_fill_continuous(labels = scales::percent) +
   facet_wrap(~resp, ncol = 1) +
-  labs(x = bquote("%"~Delta~bar(q)),
-       y = bquote("%"~Delta~bar(p))) +
-  guides(fill = guide_colorbar("Additional fuel\nconsumption (L)"),
-         size = guide_legend("% Additional fuel\nconsumption")) +
+  labs(x = bquote("%"~bar(q)),
+       y = bquote("%"~bar(p))) +
+  guides(size = guide_legend("Cost of program\n(Million MXP)"),
+         fill = guide_colorbar("% Additional fuel\nconsumption")) +
   theme(strip.text.x=element_text(margin=margin(b = 2)))
 
 
