332c2a52f783532f3b2687f6ae470f5be646791a
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-10-20 16:48:03 -0700

    Added documentation and exported new table

--- scripts/04_analysis/02_subsidy_dummies_combined.R
+++ scripts/04_analysis/02_subsidy_dummies_combined.R
@@ -1,33 +1,41 @@
-######################################################
-#title#
-######################################################
-# 
-# Purpose
+################################################################################
+# title
+################################################################################
 #
-######################################################
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
 
+## SET UP ######################################################################
+
+# Load packages ----------------------------------------------------------------
 library(here)
 library(fixest)
 library(modelsummary)
 library(tidyverse)
 
+# Load functions ---------------------------------------------------------------
 source(here("scripts", "00_setup.R"))
 
-## Load data ###################################################################
-
+# Load data --------------------------------------------------------------------
 shrimp_panel <- readRDS(file.path(project_path, "data", "processed_data", "shrimp_estimation_panel.rds")) %>% 
   mutate(tl = treated * left,
          tr = treated * right,
          nt = 1 * !treated)
 
+## PROCESSING ##################################################################
 
-## ANALYSIS ####################################################################
+# X ----------------------------------------------------------------------------
 m1 <- feols(log(fuel_consumption_l) ~ treated | eu + year, data = shrimp_panel)
 m2 <- feols(log(fuel_consumption_l) ~ treated + total_hp + predicted_subsidy_cap_l | eu + year, data = shrimp_panel)
 m3 <- feols(log(fuel_consumption_l) ~ tl + tr  + left| eu + year, data = shrimp_panel)
 m4 <- feols(log(fuel_consumption_l) ~ tl + tr + left + total_hp + predicted_subsidy_cap_l | eu + year, data = shrimp_panel)
 
-## BUILD TABLES ################################################################
+# Build tables -----------------------------------------------------------------
 m <- list(m1, m2, m3, m4)
 names(m) <- rep("log(L)", length(m))
 
@@ -39,6 +47,9 @@ controls <- c("Controls", "", "X", "", "X") %>%
   t() %>% 
   as.data.frame()
 
+## EXPORT ######################################################################
+
+# Export table -----------------------------------------------------------------
 modelsummary(models = m,
              title = "Effect of subsidy on fuel consumption.",
              # output = here("results", "tab", "subsidy_dummies_combined.tex"),
