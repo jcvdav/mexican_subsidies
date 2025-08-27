efd30584fe01275884f5504a0f9cfa2914fb26af
jcvdav <juancarlos.villader@gmail.com>
2025-08-27 15:07:02 -0400

    Added histogram ofn times subsidized

--- scripts/03_estimation/01_estimate_semi_elasticity.R
+++ scripts/03_estimation/01_estimate_semi_elasticity.R
@@ -18,7 +18,6 @@
 pacman::p_load(
   here,
   fixest,
-  # modelsummary,
   broom,
   tidyverse
 )
---
+++ scripts/05_content/make_fig_N_times_subsidized_histogram.R
@@ -0,0 +1,48 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# jc_villasenor@miami.edu
+# date
+#
+# Build a figure that a histogram of the number of times a vessel is subsidized
+#
+################################################################################
+  
+# SET UP #######################################################################
+
+## Load packages ---------------------------------------------------------------
+pacman::p_load(
+  here,
+  tidyverse
+)
+
+## Load data -------------------------------------------------------------------
+shrimp_panel <- readRDS(here("data", "estimation_panels", "shrimp_estimation_panel.rds"))
+
+# PROCESSING ###################################################################
+
+## Get unique data -------------------------------------------------------------
+data <- shrimp_panel |> 
+  select(eu, n_times_sub) |> 
+  distinct()
+
+# VISUALIZE ####################################################################
+
+## Build plot ------------------------------------------------------------------
+n_times_sub <- ggplot(data = data,
+                      mapping = aes(x = n_times_sub)) +
+  geom_histogram(binwidth = 1) +
+  scale_x_continuous(labels = c(0:9), breaks = c(0:9)) +
+  labs(x = "N times subsidized",
+       y = "# economic units") 
+
+# EXPORT #######################################################################
+## Save figure -----------------------------------------------------------------
+ggsave(plot = n_times_sub,
+       filename = here("content", "figures", "fig_N_times_subsidized_histogram.pdf"),
+       width = 6,
+       height = 4,
+       units = "in")
+
