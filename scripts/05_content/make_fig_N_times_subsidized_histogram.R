d94ed03084d2620df970526675226acf4f4065b4
jcvdav <juancarlos.villader@gmail.com>
2025-10-11 14:56:09 -0400

    Updated histogram with lines indicating samples

--- scripts/05_content/make_fig_N_times_subsidized_histogram.R
+++ scripts/05_content/make_fig_N_times_subsidized_histogram.R
@@ -26,18 +26,30 @@ shrimp_panel <- readRDS(here("data", "estimation_panels", "shrimp_estimation_pan
 
 ## Get unique data -------------------------------------------------------------
 data <- shrimp_panel |> 
-  select(eu, n_times_sub) |> 
+  select(eu, subsidy_frequency, n_times_sub) |> 
   distinct()
 
 # VISUALIZE ####################################################################
 
 ## Build plot ------------------------------------------------------------------
 n_times_sub <- ggplot(data = data,
-                      mapping = aes(x = n_times_sub)) +
+                      mapping = aes(x = n_times_sub,
+                                    fill = fct_relevel(str_to_sentence(subsidy_frequency),
+                                                       c("Never", "Sometimes", "Always")))) +
   geom_histogram(binwidth = 1) +
+  geom_linerange(y = -2, xmin = 1, xmax = 8, linewidth = 1) +
+  geom_linerange(y = -6, xmin = 2, xmax = 9, linewidth = 1, linetype = "dashed") +
+  geom_linerange(y = -10, xmin = 8.5, xmax = 9.5, linewidth = 1, linetype = "dotted") +
   scale_x_continuous(labels = c(0:9), breaks = c(0:9)) +
+  ylim(c(-10, NA)) +
+  theme(legend.position = "inside",
+        legend.position.inside = c(0, 1),
+        legend.justification.inside = c(0, 1),
+        legend.background = element_blank()) +
   labs(x = "Number of times subsidized (2011-2019)",
-       y = "Number of  economic units") 
+       y = "Number of  economic units",
+       fill = "Subsidy frequency group") +
+  scale_fill_brewer(palette = "Blues")
 
 # EXPORT #######################################################################
 ## Save figure -----------------------------------------------------------------
