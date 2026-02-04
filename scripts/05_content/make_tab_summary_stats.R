52998b74d94f0d728b0cd1ad72a3a061be0beadc
jcvdav <juancarlos.villader@gmail.com>
2026-02-04 16:25:10 -0500

    Reran with correct themes

--- scripts/05_content/make_tab_summary_stats.R
+++ scripts/05_content/make_tab_summary_stats.R
@@ -20,20 +20,17 @@ pacman::p_load(
 )
 
 # Load data --------------------------------------------------------------------
-panel <- readRDS(file = here("data", "estimation_panels", "shrimp_estimation_panel.rds"))
+panel <- readRDS(file = here("data", "estimation_panels", "shrimp_estimation_panel.rds")) |> 
+  filter(year <= 2019)
 
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
-datasummary(formula = ( subsidy_pesos + hours + fg_area_km + landed_weight) * treated ~ 
+datasummary(formula = (subsidy_pesos + hours + fg_area_km + landed_weight) * treated ~ 
               (Mean + SD + Min + Max),
             data = panel %>% mutate(treated = ifelse(treated == 1, "Subsidized", "Not subsidized")),
             format = "kableExtra",
-            output = here("results", "tab", "summary_stats.tex"))
-
-## VISUALIZE ###################################################################
-
-# X ----------------------------------------------------------------------------
+            output = here("content", "tables", "tab_summary_stats.tex"))
 
 ## EXPORT ######################################################################
 
